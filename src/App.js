e4e1528775b179ef02cfa0ac235fe5990ded4c76
octapus <24534282+octapus@users.noreply.github.com>
2021-06-23 23:09:04 -0400

    Add basic calendar with example events

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import React from 'react';
 import ISO6391 from 'iso-639-1';
 import { Translator, Translate } from 'react-auto-translate';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './App.scss';
+
+const locales = {
+  'en-US': require('date-fns/locale/en-US'),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+// javascript indexes months 0-11
+const myEventsList = [
+  {
+    title: 'My first event',
+    start: new Date(2021, 5, 22),
+    end: new Date(2021, 5, 28),
+  },
+  {
+    title: 'My second event',
+    allDay: true,
+    start: new Date(2021, 5, 22),
+    end: new Date(2021, 5, 23),
+  },
+  {
+    title: 'My third event',
+    allDay: false,
+    start: new Date(2021, 5, 12, 8),
+    end: new Date(2021, 5, 12, 12),
+  },
+  {
+    title: 'special',
+    start: new Date(),
+    end: new Date(),
+  },
+  {
+    title: 'extra',
+    start: new Date(2021, 5, 18),
+    end: new Date(2021, 5, 18),
+  },
+];
 
 class App extends React.Component {
   constructor(props) {
@@ -71,6 +118,11 @@ class App extends React.Component {
             <Translate>Hello world! Current app implementation is testing translation features. Click the dropdown to select a supported language.</Translate>
           </div>
         </Translator>
+        <Calendar
+          localizer={localizer}
+          events={myEventsList}
+          style={{ height: 500 }}
+        />
       </div>
     );
   }
